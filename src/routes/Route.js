bb69987af8b24581706912adeff13ac0d8220e07
ArjDev69 <bruno96araujo@gmail.com>
2020-06-24 17:02:21 -0300

    format

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,38 +1,45 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Redirect, Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom';
 
-import AuthLayout from '~/pages/_layout/auth'
-import DefaultLayout from '~/pages/_layout/default'
+import AuthLayout from '~/pages/_layout/auth';
+import DefaultLayout from '~/pages/_layout/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
-}){
-  const signed = true;
+}) {
+  const signed = false;
 
-  if(!signed && isPrivate){
+  if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
 
-  if(signed && !isPrivate){
-    return <Redirect to="/dashboard" />
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
   }
 
   const Layout = signed ? DefaultLayout : AuthLayout;
 
-  return <Route {...rest} render={props => (
-    <Layout>
-      <Component {...props} />
-    </Layout>
-  )} />;
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
   isPrivate: PropTypes.bool,
-  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
 };
 
 RouteWrapper.defaultProps = {
   isPrivate: false,
-}
+};

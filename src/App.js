984590ddc9580668baccd86b9de03d357e41f1ce
ArjDev69 <bruno96araujo@gmail.com>
2020-06-26 14:19:58 -0300

    created session with api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './config/ReactotronConfig';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Routes from './routes';
 
-import './config/ReactotronConfig';
+import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import store from '~/store';
 
 import GlobalStyle from './Styles/global';
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
-import SignIn from '../pages/SignIn'
-import SignUp from '../pages/SignUp'
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
-import Dashboard from '../pages/Dashboard'
-import Profile from '../pages/Profile'
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
-import Route from './Route'
+import Route from './Route';
 
 function Routes() {
-  return(
+  return (
     <Switch>
-        <Route path="/" exact component={SignIn} />
-        <Route path="/register" exact component={SignUp} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" exact component={SignUp} />
 
-        <Route path="/dashboard" isPrivate component={Dashboard} />
-        <Route path="/profile"  isPrivate component={Profile} />
+      <Route path="/dashboard" isPrivate component={Dashboard} />
+      <Route path="/profile" isPrivate component={Profile} />
     </Switch>
   );
 }

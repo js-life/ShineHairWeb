8a403e9c384e7363bf3687068f0ce4b50c64c888
ArjDev69 <bruno96araujo@gmail.com>
2020-06-26 15:27:33 -0300

    loading auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
-
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
-import logo from '~/assets/logo-white.svg';
 
 import { signInRequest } from '~/store/modules/auth/actions';
 
+import logo from '~/assets/logo-white.svg';
+
 const schema = Yup.object().shape({
   email: Yup.string()
-    .email('Insira um email válido')
-    .required('O email é obrigatório'),
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
   password: Yup.string().required('A senha é obrigatória'),
 });
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -28,10 +29,14 @@ export default function SignIn() {
 
       <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
-        <Input name="password" type="password" placeholder="Sua senha..." />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
-        <button type="submit">Acessar</button>
-        <Link to="/register">Criar conta</Link>
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
   );

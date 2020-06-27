271168dc51edbb10be3b0710ab2a076e01b20b15
ArjDev69 <bruno96araujo@gmail.com>
2020-06-27 20:30:27 -0300

    fix layout

--- src/pages/_layout/default/styles.js
+++ src/pages/_layout/default/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  height: auto;
+  padding-bottom: 15px;
   background: linear-gradient(-90deg, #058996, #02585569);
 `;
